1da095305ef71e0e12977a58c42dc14cfc1aa574
Abiria <oro3673@gmail.com>
2025-10-18 00:35:00 -0400

    build: packaging with nix

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "O(log n) span highlighter";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      with pkgs;
+      {
+        packages.default = rustPlatform.buildRustPackage (_: {
+          pname = "highlight-span";
+          version = "0.1.0";
+          src = ./.;
+
+          cargoLock.lockFile = ./Cargo.lock;
+        });
+
+        devShells.default = mkShell {
+          packages = [
+            cargo
+          ];
+        };
+      }
+    );
+}
