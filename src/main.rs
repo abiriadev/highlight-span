df243c318eaab864141c5c37d607ff1c300036e0
Abiria <oro3673@gmail.com>
2025-11-09 06:12:34 -0500

    feat: read source from stdin if arg isn't provided

--- src/main.rs
+++ src/main.rs
@@ -5,27 +5,47 @@ use highlight_span::{Highlighter, ToTokenName};
 struct TokenDesc(String);
 
 impl ToTokenName for TokenDesc {
-	fn to_token_name(&self) -> String { self.0.clone() }
+	fn to_token_name(&self) -> String {
+		self.0.clone()
+	}
+}
+
+fn is_delimiter(line: &str) -> bool {
+	line.chars().all(|c| c == '=') && line.len() >= 10
 }
 
 fn main() {
-	let spans = stdin()
-		.lines()
-		.map(Result::unwrap)
-		.map(|s| {
-			let (a, b) = s.split_once(' ').unwrap();
-			let (b, c) = b.split_once(' ').unwrap();
+	let mut source = String::new();
 
-			let a = a.parse::<usize>().unwrap();
-			let b = b.parse::<usize>().unwrap();
-			let c = TokenDesc(c.to_owned());
+	match args().nth(1) {
+		// read source from provided file.
+		Some(filepath) => source = read_to_string(filepath).unwrap(),
+		_ => {
+			// read from stdin until finding a line with 10 or more '=' characters
+			for line in stdin().lines() {
+				let line = line.unwrap();
+				if is_delimiter(&line) {
+					break;
+				}
 
-			(a, b, c)
-		});
+				source.push_str(&line);
+				source.push('\n');
+			}
+		},
+	}
 
-	let src = read_to_string(args().nth(1).unwrap()).unwrap();
+	let spans = stdin().lines().map(Result::unwrap).map(|s| {
+		let (a, b) = s.split_once(' ').unwrap();
+		let (b, c) = b.split_once(' ').unwrap();
 
-	let mut hl = Highlighter::new(&src, "\n", 4);
+		let a = a.parse::<usize>().unwrap();
+		let b = b.parse::<usize>().unwrap();
+		let c = TokenDesc(c.to_owned());
+
+		(a, b, c)
+	});
+
+	let mut hl = Highlighter::new(&source, "\n", 4);
 
 	for (a, b, c) in spans {
 		hl.next_token(c, a..b);
